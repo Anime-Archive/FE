58dbdf8bc1b54ade527566643ab636f8dfe19b76
Erik Rodriguez <perhapserik@gmail.com>
2021-10-19 19:43:34 -0700

    • Updates dummy data for anime entry format

--- app/src/DummyData/CarouselData.js
+++ app/src/DummyData/CarouselData.js
@@ -1,124 +1,5 @@
 const carouselData = [
   {
-    id: 127401,
-    coverImage: {
-      extraLarge:
-        "https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx127401-fVKVbuIE5W5Q.jpg",
-    },
-    title: {
-      userPreferred: "Platinum End",
-      english: "Platinum End",
-    },
-    averageScore: null,
-    popularity: 34920,
-    genres: ["Drama", "Mystery", "Psychological", "Romance", "Supernatural"],
-    studios: {
-      nodes: [
-        {
-          name: "Signal.MD",
-        },
-      ],
-    },
-    description:
-      "When life becomes too much for Mirai to bear and attempts taking his own life, he’s saved by Nasse, a guardian angel who offers him a chance at becoming the new god. To do so, he must face off against 12 other candidates within 999 days. Mirai, who was once at rock bottom, now finds himself with supernatural powers in a battle royale for the highest title.\n<br><br>\n(Source: Funimation)",
-  },
-  {
-    id: 133965,
-    coverImage: {
-      extraLarge:
-        "https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx133965-1jyaEkgXQW8a.jpg",
-    },
-    title: {
-      userPreferred: "Komi-san wa, Komyushou desu.",
-      english: "Komi Can’t Communicate",
-    },
-    averageScore: 80,
-    popularity: 54104,
-    genres: ["Comedy", "Romance", "Slice of Life"],
-    studios: {
-      nodes: [
-        {
-          name: "OLM",
-        },
-      ],
-    },
-    description:
-      "Timid Tadano is a total wallflower, and that’s just the way he likes it. But all that changes when he finds himself alone in a classroom on the first day of high school with the legendary Komi. He quickly realizes she isn’t aloof—she’s just super awkward. Now he’s made it his mission to help her on her quest to make 100 friends!<br>\n<br>\n(Source: Viz Media)",
-  },
-  {
-    id: 108987,
-    coverImage: {
-      extraLarge:
-        "https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx108987-rdPglVthrZP2.jpg",
-    },
-    title: {
-      userPreferred: "Sakugan!!",
-      english: "SAKUGAN",
-    },
-    averageScore: null,
-    popularity: 7435,
-    genres: ["Adventure", "Mecha", "Sci-Fi"],
-    studios: {
-      nodes: [
-        {
-          name: "Satelight",
-        },
-      ],
-    },
-    description:
-      'In the distant future, humankind lives shoulder-to-shoulder in a cramped "Colony" divided by bedrock. Outside the colony extends a dangerous undeveloped area known as "The Labyrinth." Those who risk their lives to explore the Labyrinth and mark undeveloped areas are known as "Markers." Memempu is a young girl who wants to become a Marker, and Gagumber is a man who quit being a Marker. This rag-tag father and daughter team are about to take on the Labyrinth! "If there is no path, then dig one!\n<br><br>\n(Source: Crunchyroll)\n<br><br>\n<i>Note: Episode 1 had an early screening during Virtual Crunchyroll Expo 2021 on August 5, 2021.</i>',
-  },
-  {
-    id: 128545,
-    coverImage: {
-      extraLarge:
-        "https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx128545-8ooIQffo7elA.png",
-    },
-    title: {
-      userPreferred: "Shiroi Suna no Aquatope",
-      english: "The Aquatope on White Sand",
-    },
-    averageScore: 72,
-    popularity: 31994,
-    genres: ["Drama", "Slice of Life", "Supernatural"],
-    studios: {
-      nodes: [
-        {
-          name: "P.A. Works",
-        },
-      ],
-    },
-    description:
-      'The anime takes place at Gama Gama Aquarium, a small aquarium in Okinawa, an hour\'s bus ride from Naha. Kukuru Misakino is an 18-year-old high school student who works there, and she knows about the "secret" of the aquarium: sometimes you can see mysterious things. One day Kukuru meets Fuuka, standing in front of a water tank with flowing hair and a tear rolling down her cheek. Fuuka has given up on her dream of becoming an idol and she has run away from Tokyo to Okinawa. Wanting to find a place to belong, Fuuka earnestly asks to work at the aquarium. The anime follows Kukuru and Fuuka as they deal with the issues of the secret of the aquarium and a looming crisis of its possible closure.<br>\n<br>\n(Source: Anime News Network)',
-  },
-  {
-    id: 129898,
-    coverImage: {
-      extraLarge:
-        "https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx129898-FRUzDtPhRigt.jpg",
-    },
-    title: {
-      userPreferred: "Sekai Saikou no Ansatsusha, Isekai Kizoku ni Tensei suru",
-      english:
-        "The World's Finest Assassin Gets Reincarnated in Another World as an Aristocrat",
-    },
-    averageScore: 72,
-    popularity: 24368,
-    genres: ["Action", "Adventure", "Drama", "Fantasy", "Mystery", "Romance"],
-    studios: {
-      nodes: [
-        {
-          name: "SILVER LINK.",
-        },
-        {
-          name: "Studio Palette",
-        },
-      ],
-    },
-    description:
-      "The world's number one assassin has been reincarnated as the eldest son of a family of aristocrat assassins. In exchange for being reincarnated in another world, a goddess has imposed upon him one condition. \"Kill the Hero who is prophesied to destroy the world.\" <br>\n<br>\nThis was to be the mission in his new life. The synergistic effect of the vast knowledge and experience he gained that made all manner of assassinations possible in the modern world, and the secret techniques and magic of the fantasy world's most powerful family of assassins turn him into the greatest assassin of all time.<br>\n<br>\n(Source: Crunchyroll)",
-  },
-  {
     id: 21,
     coverImage: {
       extraLarge:
@@ -129,7 +10,7 @@ const carouselData = [
       english: "One Piece",
     },
     averageScore: 86,
-    popularity: 277305,
+    popularity: 280635,
     genres: ["Action", "Adventure", "Comedy", "Drama", "Fantasy"],
     studios: {
       nodes: [
@@ -138,76 +19,33 @@ const carouselData = [
         },
       ],
     },
+    format: "TV",
     description:
       "Gold Roger was known as the Pirate King, the strongest and most infamous being to have sailed the Grand Line. The capture and death of Roger by the World Government brought a change throughout the world. His last words before his death revealed the location of the greatest treasure in the world, One Piece. It was this revelation that brought about the Grand Age of Pirates, men who dreamed of finding One Piece (which promises an unlimited amount of riches and fame), and quite possibly the most coveted of titles for the person who found it, the title of the Pirate King.<br><br>\nEnter Monkey D. Luffy, a 17-year-old boy that defies your standard definition of a pirate. Rather than the popular persona of a wicked, hardened, toothless pirate who ransacks villages for fun, Luffy’s reason for being a pirate is one of pure wonder; the thought of an exciting adventure and meeting new and intriguing people, along with finding One Piece, are his reasons of becoming a pirate. Following in the footsteps of his childhood hero, Luffy and his crew travel across the Grand Line, experiencing crazy adventures, unveiling dark mysteries and battling strong enemies, all in order to reach One Piece.<br><br>\n<b>*This includes following special episodes:</b><br>\n- Chopperman to the Rescue! Protect the TV Station by the Shore! (Episode 336)<br>\n- The Strongest Tag-Team! Luffy and Toriko's Hard Struggle! (Episode 492)<br>\n- Team Formation! Save Chopper (Episode 542)<br>\n- History's Strongest Collaboration vs. Glutton of the Sea (Episode 590)<br>\n- 20th Anniversary! Special Romance Dawn (Episode 907)",
   },
   {
-    id: 132193,
+    id: 129874,
     coverImage: {
       extraLarge:
-        "https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx132193-xDRqOqW8eWaI.jpg",
+        "https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx129874-R5sdioCVMKwe.png",
     },
     title: {
-      userPreferred: "Gokushufudou Part 2",
-      english: "The Way of the Househusband Part 2",
+      userPreferred: "Kimetsu no Yaiba 2",
+      english: "Demon Slayer: Kimetsu no Yaiba 2",
     },
-    averageScore: 59,
-    popularity: 9762,
-    genres: ["Comedy", "Slice of Life"],
+    averageScore: 84,
+    popularity: 89224,
+    genres: ["Action", "Adventure", "Drama", "Fantasy", "Supernatural"],
     studios: {
       nodes: [
         {
-          name: "J.C. Staff",
+          name: "ufotable",
         },
       ],
     },
-    description: "The second part of <i>Gokushufudou</i>.",
-  },
-  {
-    id: 131083,
-    coverImage: {
-      extraLarge:
-        "https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx131083-fMWFyOFgp6vb.png",
-    },
-    title: {
-      userPreferred: "Mieruko-chan",
-      english: "Mieruko-chan",
-    },
-    averageScore: 72,
-    popularity: 33590,
-    genres: ["Comedy", "Horror", "Slice of Life", "Supernatural"],
-    studios: {
-      nodes: [
-        {
-          name: "Passione",
-        },
-      ],
-    },
+    format: "TV",
     description:
-      "She can see dead people…she just chooses to ignore them. That’s Miko’s plan, anyway, but it doesn’t seem to be working for her in this love letter to the horror genre that will send shivers down your spine—and occasionally tickle your funny bone.<br>\n<br>\n(Source: Funimation)",
-  },
-  {
-    id: 120120,
-    coverImage: {
-      extraLarge:
-        "https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx120120-uMy65OVFG1mO.png",
-    },
-    title: {
-      userPreferred: "Tokyo Revengers",
-      english: "Tokyo Revengers",
-    },
-    averageScore: 83,
-    popularity: 173546,
-    genres: ["Action", "Drama", "Romance", "Supernatural"],
-    studios: {
-      nodes: [
-        {
-          name: "LIDENFILMS",
-        },
-      ],
-    },
-    description:
-      "Takemichi Hanagaki is a freelancer that's reached the absolute pits of despair in his life. He finds out that the only girlfriend he ever had in his life that he dated in middle school, Hinata Tachibana, had been killed by the ruthless Tokyo Manji Gang.<br>\n<br>\nThe day after hearing about her death, he's standing on the station platform and ends up being pushed over onto the tracks by a herd of people. He closes his eyes thinking he's about to die, but when he opens his eyes back up, he somehow had gone back in time 12 years.<br>\n<br>\nNow that he's back living the best days of his life, Takemichi decides to get revenge on his life by saving his girlfriend and changing himself that he'd been running away from.<br>\n<br>\n(Source: Crunchyroll)",
+      "The second season of <i>Kimetsu no Yaiba</i>.<br>\n<br>\n<i>Notes:<br>\n- The first seven episodes of this entry is the TV version of Kimetsu no Yaiba: Mugen Ressha-hen.<br>\n- The first episode (10 October) is entirely anime original <br>\n- The eighth episode (5 December) airs with a runtime of ~1 hour.</i>",
   },
   {
     id: 127720,
@@ -220,7 +58,7 @@ const carouselData = [
       english: "Mushoku Tensei: Jobless Reincarnation Part 2",
     },
     averageScore: 83,
-    popularity: 61736,
+    popularity: 69230,
     genres: ["Adventure", "Drama", "Ecchi", "Fantasy"],
     studios: {
       nodes: [
@@ -229,8 +67,179 @@ const carouselData = [
         },
       ],
     },
+    format: "TV",
     description:
-      "The second cour of <i>Mushoku Tensei: Isekai Ittara Honki Dasu</i>.<br>\n<br>\nRudy, Eris, and Rujierd head for Asura Kingdom in search of answers. Meanwhile, Roxy sets out across the Demon Continent to find Rudy.<br>\n<br>\n(Source: Funimation)",
+      "Rudy, Eris, and Rujierd head for Asura Kingdom in search of answers. Meanwhile, Roxy sets out across the Demon Continent to find Rudy.<br>\n<br>\n(Source: Funimation)",
+  },
+  {
+    id: 133965,
+    coverImage: {
+      extraLarge:
+        "https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx133965-1jyaEkgXQW8a.jpg",
+    },
+    title: {
+      userPreferred: "Komi-san wa, Komyushou desu.",
+      english: "Komi Can’t Communicate",
+    },
+    averageScore: 82,
+    popularity: 67049,
+    genres: ["Comedy", "Romance", "Slice of Life"],
+    studios: {
+      nodes: [
+        {
+          name: "OLM",
+        },
+      ],
+    },
+    format: "TV",
+    description:
+      "Timid Tadano is a total wallflower, and that’s just the way he likes it. But all that changes when he finds himself alone in a classroom on the first day of high school with the legendary Komi. He quickly realizes she isn’t aloof—she’s just super awkward. Now he’s made it his mission to help her on her quest to make 100 friends!<br>\n<br>\n(Source: Viz Media)",
+  },
+  {
+    id: 131083,
+    coverImage: {
+      extraLarge:
+        "https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx131083-fMWFyOFgp6vb.png",
+    },
+    title: {
+      userPreferred: "Mieruko-chan",
+      english: "Mieruko-chan",
+    },
+    averageScore: 71,
+    popularity: 42766,
+    genres: ["Comedy", "Horror", "Slice of Life", "Supernatural"],
+    studios: {
+      nodes: [
+        {
+          name: "Passione",
+        },
+      ],
+    },
+    format: "TV",
+    description:
+      "She can see dead people…she just chooses to ignore them. That’s Miko’s plan, anyway, but it doesn’t seem to be working for her in this love letter to the horror genre that will send shivers down your spine—and occasionally tickle your funny bone.<br>\n<br>\n(Source: Funimation)",
+  },
+  {
+    id: 128705,
+    coverImage: {
+      extraLarge:
+        "https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx128705-2PzZbN52XCGI.jpg",
+    },
+    title: {
+      userPreferred: "Blue Period",
+      english: "Blue Period",
+    },
+    averageScore: 74,
+    popularity: 42195,
+    genres: ["Drama", "Slice of Life"],
+    studios: {
+      nodes: [
+        {
+          name: "Seven Arcs",
+        },
+      ],
+    },
+    format: "ONA",
+    description:
+      "The studious Yatora leaves a dry life of study and good manners behind for a new passion: painting. But untethering yourself from all your past expectations is dangerous as well as thrilling…\n<br><br>\n(Source: Kodansha USA)<br>\n<br>\n<i>Note: The anime received an early premiere on Netflix on Sep 25, 2021. The regular TV broadcast started on Oct 2, 2021.</i>",
+  },
+  {
+    id: 129068,
+    coverImage: {
+      extraLarge:
+        "https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx129068-CLyk6Kh2nBQl.jpg",
+    },
+    title: {
+      userPreferred: "Shinka no Mi: Shiranai Uchi ni Kachigumi Jinsei",
+      english: "The Fruit of Evolution: Before I Knew It, My Life Had It Made",
+    },
+    averageScore: 62,
+    popularity: 12321,
+    genres: ["Adventure", "Comedy", "Fantasy", "Romance"],
+    studios: {
+      nodes: [
+        {
+          name: "Children's Playground Entertainment",
+        },
+        {
+          name: "feel.",
+        },
+      ],
+    },
+    format: "TV",
+    description:
+      "Hiiragi Seiichi is an ugly, revolting, dirty, smelly fatass; these are the insults hurled at him one after another about his appearance. Such was Seiichi's daily school life of bullying, then for some reason, one day when school was out, a voice claiming to be a God said over the PA system to prepare to be transported to another world. What's more, not Seiichi alone, but the entire school. A fantasy world where game-like elements such as as levels, stats, and skills exist. However, the God still had preparations to complete for the transfer, and would send them over as soon as the hero summoning ritual was ready. The classes all formed groups to wait for the transfer, but Seiichi alone was discluded and as such was summoned to a different area. After being transported the first thing Seiichi ate was the \"Fruit of Evolution\". This would come to greatly change his life\n<br><br>\nThis story is about how Seiichi went from being severely bullied by his classmates, even not being recognized for his accomplishments, and despite all that staying positive and surviving in this new world. As a result, he somehow becomes one of the champions.",
+  },
+  {
+    id: 1735,
+    coverImage: {
+      extraLarge:
+        "https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx1735-80JNLAlnxrKj.png",
+    },
+    title: {
+      userPreferred: "Naruto: Shippuuden",
+      english: "Naruto: Shippuden",
+    },
+    averageScore: 82,
+    popularity: 289255,
+    genres: ["Action", "Comedy"],
+    studios: {
+      nodes: [
+        {
+          name: "Studio Pierrot",
+        },
+      ],
+    },
+    format: "TV",
+    description:
+      "Naruto: Shippuuden is the continuation of the original animated TV series Naruto. The story revolves around an older and slightly more matured Uzumaki Naruto and his quest to save his friend Uchiha Sasuke from the grips of the snake-like Shinobi, Orochimaru. After 2 and a half years Naruto finally returns to his village of Konoha, and sets about putting his ambitions to work, though it will not be easy, as he has amassed a few (more dangerous) enemies, in the likes of the shinobi organization; Akatsuki. <br><br>\n(Source: Anime News Network)",
+  },
+  {
+    id: 131586,
+    coverImage: {
+      extraLarge:
+        "https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx131586-k0X2kVpUOkqX.jpg",
+    },
+    title: {
+      userPreferred: "86: Eighty Six Part 2",
+      english: "86 EIGHTY-SIX Season 2",
+    },
+    averageScore: 82,
+    popularity: 35413,
+    genres: ["Action", "Drama", "Mecha", "Sci-Fi"],
+    studios: {
+      nodes: [
+        {
+          name: "A-1 Pictures",
+        },
+      ],
+    },
+    format: "TV",
+    description: "The second cour of <i>86: Eighty Six</i>.",
+  },
+  {
+    id: 127401,
+    coverImage: {
+      extraLarge:
+        "https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx127401-fVKVbuIE5W5Q.jpg",
+    },
+    title: {
+      userPreferred: "Platinum End",
+      english: "Platinum End",
+    },
+    averageScore: 72,
+    popularity: 48065,
+    genres: ["Drama", "Mystery", "Psychological", "Romance", "Supernatural"],
+    studios: {
+      nodes: [
+        {
+          name: "Signal.MD",
+        },
+      ],
+    },
+    format: "TV",
+    description:
+      "When life becomes too much for Mirai to bear and attempts taking his own life, he’s saved by Nasse, a guardian angel who offers him a chance at becoming the new god. To do so, he must face off against 12 other candidates within 999 days. Mirai, who was once at rock bottom, now finds himself with supernatural powers in a battle royale for the highest title.\n<br><br>\n(Source: Funimation)",
   },
 ];
 
